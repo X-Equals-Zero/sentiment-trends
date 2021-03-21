49204a512ff74b386f988b1c895146969ee6cd12
X-Equals-Zero <j.svitlik@gmail.com>
2021-03-21 03:33:36 -0400

    git mistake fix

--- scrapeYT.py
+++ scrapeYT.py
@@ -4,18 +4,40 @@ import json
 import sys
 
 QUERY = 'crypto'
-KEY = 'inser ur key here'
+KEYFILE = open('.key.txt', 'r')
+KEY = KEYFILE.read()
+KEYFILE.close()
+print(KEY)
+MAXRES = '50'
+NEXTPAGE = ''
+
 if len(sys.argv) > 1:
     QUERY = sys.argv[1]
 
+def nextPage(page):
+    
 
+    print("TRIGGERED ===============================")
+    print(str(len(page)))
+    if len(page) > 1:
+        
+        return('&pageToken=' + page)
+    else:
+        return('')
 
-url ='https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + QUERY + '&key='+ KEY + '&maxResults=50'
+def url():
+    temp = 'https://www.googleapis.com/youtube/v3/search?part=snippet' + nextPage(NEXTPAGE) + '&q=' + QUERY + '&key='+ KEY + '&maxResults=' + MAXRES
+    return(temp)
 
-print(url)
-print(url)
-print(url)
-x = requests.get(url, headers={'User-Agent': 'Custom'}).json()
+x = requests.get(url(), headers={'user-agent': 'custom'}).json()
+NEXTPAGE = x['nextPageToken']
+
+print(NEXTPAGE + "<-- NEXT page url")
+x1 = requests.get(url(), headers={'user-agent': 'custom'}).json()
+print(url())
+
 df = pd.DataFrame(pd.json_normalize(x['items']))
+df = df.append(pd.DataFrame(pd.json_normalize(x1['items'])))
 print(df.head())
+
 df.to_csv('query.csv', index=False)
